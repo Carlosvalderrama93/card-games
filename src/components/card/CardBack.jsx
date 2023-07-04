e1c41a2323796878b19c60bbf0a662689c35c5d8
Charlie  Valderrama <carlosvalderrama@onmail.com>
2023-07-04 09:25:44 -0500

    CardBack component and its test implemented correctly

--- src/components/card/CardBack.jsx
+++ src/components/card/CardBack.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import propsValidator from "../../Others/propsValidator";
 
-function CardBack({ type }) {
-  let cardBack;
-  type === "one"
-    ? (cardBack = <img src="/card-back-one.svg" alt="CardBack One" />)
-    : (cardBack = <img src="/card-back-two.svg" alt="CardBack Two" />);
+function cardBack({ color }) {
+  const isValid = propsValidator({ color }, 1);
+  if (!isValid) return null;
+  const isBlue = color === "blue";
+  const cardBlue = (
+    <div className="card-back">
+      <img src="/card-back-blue.svg" alt="cardColor blue" />
+    </div>
+  );
+  const cardRed = (
+    <div className="card-back">
+      <img src="/card-back-red.svg" alt="cardColor red" />
+    </div>
+  );
 
-  return <div className="card-back">{cardBack}</div>;
+  return isBlue ? cardBlue : cardRed;
 }
 
-export default CardBack;
+export default cardBack;

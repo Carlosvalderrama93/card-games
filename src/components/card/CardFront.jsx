1ce57ca358a9a7917dd7cdfcca50a2b17e7aabb4
Charlie  Valderrama <carlosvalderrama@onmail.com>
2023-07-04 08:37:52 -0500

    CardFront component and its test implemented correctly

--- src/components/card/CardFront.jsx
+++ src/components/card/CardFront.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+
+import propsValidator from "../../Others/propsValidator";
 import FigureMain from "../icons/FigureMain";
+import CardCorner from "./CardCorner";
 import IconMain from "../icons/IconMain";
 import Title from "../Title";
 
 function CardFront(props) {
-  const { title, type } = props;
+  const areValid = propsValidator(props, 2);
+  if (!areValid) return null;
+  
   return (
     <div className="card-front">
-      <div className="card-front-title">
-        <Title>{title}</Title>
-        <IconMain type={type} />
-      </div>
-      <div className="card-front-img">
-        <FigureMain type={type} />
-      </div>
-      <div className="card-front-title">
-        <Title>{title}</Title>
-        <IconMain type={type} />
-      </div>
+      <CardCorner>
+        <Title>{props.rank}</Title>
+        <IconMain suite={props.suite} />
+      </CardCorner>
+      <FigureMain suite={props.suite} />
+      <CardCorner>
+        <Title>{props.rank}</Title>
+        <IconMain suite={props.suite} />
+      </CardCorner>
     </div>
   );
 }

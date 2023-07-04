053065d617525e68ae46129e1157c7b9a34b6f12
Charlie  Valderrama <carlosvalderrama@onmail.com>
2023-07-04 10:55:02 -0500

    App arguments  fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Card from "./components/card/Card";
 
 function App() {
-  return <Card title={"A"} typeFront={"club"} typeBack={"one"} />;
+  return <Card rank={"A"} suite={"club"} color={"blue"} />;
 }
 
 export default App;

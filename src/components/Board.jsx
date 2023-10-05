cd2d0c327589a5f3bb66ba088cbf990012446665
Charlie  Valderrama <carlosvalderrama@onmail.com>
2023-10-04 21:26:11 -0500

    Component  optimized to work with the new component 'DrawHands', previously commited.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 
 import startGame from "../Others/startGame";
-import DrawHand from "./DrawHand";
+import DrawHands from "./DrawHands";
 
 function Board() {
-  const { players } = startGame(2);
+  const { players } = startGame();
 
   return (
-    <div className="board-cards">
-      <DrawHand hand={players[0].hand.cards} />
+    <div className="board">
       {players.map((player) => (
-        <div key={player.id}>
-          <h2>{player.name}</h2>
-          <h4>Player Score: {player.hand.score ?? "Null"}</h4>
-          <div>
-            <h5>
-              {player.hand.cards.map((card) => (
-                <p key={card.id}>{card.rank + " of " + card.suite}</p>
-              ))}
-            </h5>
-          </div>
-        </div>
+        <DrawHands hand={player.hand} key={player.id} />
       ))}
+      <div className="control-panel">Control Panel</div>
     </div>
   );
 }

04f3628a2a90cb8ef5647e9384f54de5e53bbd02
Charlie  Valderrama <carlosvalderrama@onmail.com>
2023-10-19 13:28:45 -0500

    Was added an state to restrict when can be fliped the ocult card of Dealer.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,21 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 import startGame from "../Others/startGame";
 import DrawHands from "./DrawHands";
 
 function Board() {
+  const [flip, setFlip] = useState(false);
   const { players, deck } = startGame();
   const refDeck = useRef(deck);
 
   return (
     <div className="board">
       {players.map((player) => (
-        <DrawHands player={player} deck={refDeck} />
+        <DrawHands
+          player={player}
+          deck={refDeck}
+          flip={{ state: flip, set: setFlip }}
+        />
       ))}
     </div>
   );
